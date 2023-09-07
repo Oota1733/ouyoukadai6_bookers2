b28ce12f926c1cb89f9f926e1bee0e8bdece2ea6
EC2 Default User <ec2-user@ip-172-31-25-120.ap-northeast-1.compute.internal>
2023-09-07 06:19:33 +0000

    Finish

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,23 @@
 class CommentsController < ApplicationController
 
   def create
-    current_user.follow(params[:user_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = params[:book_id]
+    @comment.save
+    redirect_to request.referer
+
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
     redirect_to request.referer
   end
-  
-  def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
-  
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
- 
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
-  end
+
 
 end
